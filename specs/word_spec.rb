2e0c618b15845d0abbf5ed9d38577563e04ac905
DeepCFoxXx <learncode23@gmail.com>
2018-03-24 17:35:01 +0000

    reveals whole word passing

--- specs/word_spec.rb
+++ specs/word_spec.rb
@@ -21,4 +21,9 @@ class WordTest < MiniTest::Test
     assert_equal("t**t", revealed_t)
   end
 
+  def test_reveals_whole_word
+    revealed_word = @word.display(['t', 'e', 's'])
+    assert_equal("test", revealed_word)
+  end
+
 end
